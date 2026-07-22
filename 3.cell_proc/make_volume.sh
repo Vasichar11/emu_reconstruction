3b5d36453864faf3f237f53255378538936fc574
Vasileios Chariton <vasileios.chariton@cern.ch>
2026-07-22 10:15:33 +0200

    Filter cells option

--- 3.cell_proc/make_volume.sh
+++ 3.cell_proc/make_volume.sh
@@ -1,11 +1,25 @@
 BRICKID=$1
 BRICKFOLDER="$(printf "b%0*d" 6 $BRICKID)"
+
+# === CELL FILTER LIMITS (comment all to disable filtering) ===
+#XPOS_MIN=5000
+#XPOS_MAX=35000
+#YPOS_MIN=75000
+#YPOS_MAX=125000
+
 mkdir report_trk
 for CELL in $(seq 0 323); do
   xcell=$(((CELL % 18 + 1) * 10))
   ycell=$(((CELL / 18 + 1) * 10))
   xpos=$((xcell * 1000))
   ypos=$((ycell * 1000))
+  
+  # Apply filter only if limits are defined
+  if [ -n "$XPOS_MIN" ] && [ -n "$XPOS_MAX" ] && [ -n "$YPOS_MIN" ] && [ -n "$YPOS_MAX" ]; then
+    if [ $xpos -lt $XPOS_MIN ] || [ $xpos -gt $XPOS_MAX ] || [ $ypos -lt $YPOS_MIN ] || [ $ypos -gt $YPOS_MAX ]; then
+      continue
+    fi
+  fi
   folder=cell_${xcell}_${ycell}
 
   if [ ! -d "$folder" ]; then
