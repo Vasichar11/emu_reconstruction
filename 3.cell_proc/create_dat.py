e57ead032c5b894e6c8e2d3feaef0edbb2a69694
Vasileios Chariton <vasileios.chariton@cern.ch>
2026-07-22 10:27:09 +0200

    Create dat, filter cells

--- 3.cell_proc/create_dat.py
+++ 3.cell_proc/create_dat.py
@@ -1,4 +1,11 @@
+# Scanned area in microns
+X_MIN, X_MAX = 5000, 35000
+Y_MIN, Y_MAX = 75000, 125000
+
 with open("volumes.dat", "w+") as file:
     for plate in range(1, 58):
-        for cell in range(0,324):
-            file.write(f"{plate},{cell}\n")
+        for cell in range(0, 324):
+            xpos = (cell % 18 + 1) * 10000
+            ypos = (cell // 18 + 1) * 10000
+            if X_MIN <= xpos <= X_MAX and Y_MIN <= ypos <= Y_MAX:
+                file.write(f"{plate},{cell}\n")
